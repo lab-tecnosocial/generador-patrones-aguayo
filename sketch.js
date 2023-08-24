b75aafb91791ea3589c366a9c3e92a55046f08da
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2023-08-23 22:16:25 -0400

    nuevas funciones iniciales

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
-let cursorX = 0;
+let posicionX = 0;
 
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(900, 900);
     background(0);
-    noStroke();
     const botonGenerar = select('#generate');
     botonGenerar.mousePressed(() => {
-        patron1(cursorX);
+        patronHebra1(posicionX);
     });
 }
 
@@ -14,17 +13,44 @@ function draw() {
 
 }
 
-function patron1(inicio) {
-    let paleta = ['red', 'orange', 'yellow', 'orange', 'red'];
-    franja(0 + inicio);
-    for (let i = 0; i < 50; i += 10) {
-        fill(paleta[i / 10]);
-        rect(inicio + 20 + i, 0, 10, height);
+
+
+// piecewiese functions for fabric patterns
+function patronHebra1(inicio) {
+    let paleta = paletasRandom();
+    let franja = franjasRandom();
+    for (let i = 0; i < 90; i += 1) {
+
+        if (i < 20 || i >= 70) {
+            stroke(franja);
+            line(i + inicio, 0, i + inicio, height);
+        }
+        if (i >= 20 && i < 70) {
+            stroke(paleta[funcionEscalonada(i, 20, 10)]);
+            line(i + inicio, 0, i + inicio, height);
+        }
+
     }
-    franja(70 + inicio);
-    function franja(i) {
-        fill('skyblue');
-        rect(i, 0, 20, height);
-    }
-    cursorX = cursorX + 90;
+    posicionX = posicionX + 90;
+}
+
+// color palettes functions
+function paletasRandom() {
+    const paletas = [
+        ['red', 'orange', 'yellow', 'orange', 'red'],
+        ['blue', 'skyblue', 'white', 'skyblue', 'blue'],
+        ['green', 'lightgreen', 'white', 'lightgreen', 'green'],
+    ];
+    return paletas[Math.floor(Math.random() * paletas.length)];
+}
+
+function franjasRandom() {
+    const franjas = ['skyblue', 'crimson', 'green'];
+    return franjas[Math.floor(Math.random() * franjas.length)];
+
+}
+
+// utilities functions
+function funcionEscalonada(x, inset, step) {
+    return Math.floor((x - inset) / step);
 }
