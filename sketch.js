b7b3972ec98215f47709efab35d23876087cf714
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2023-09-25 20:51:16 -0400

    Updates

--- sketch.js
+++ sketch.js
@@ -1,19 +1,23 @@
-// (33 * 3) + (99 * 2) + (99 * 2) + (99 * 2) + (99 * 2) (33 * 3) = 990
+// layout: (24 * 3) + (72 * 2) + (72 * 2) + (72 * 2) + (72 * 2) + (24 * 3) = 720
+// relleno: patron0 * 3, patron1 + patron0 + patron1, patron2 + patron0 + patron3 + patron0 + patron2, patron1 + patron0 + patron1, patron0 * 3
+// patron0 = 24px
+// patron1 = 72px
+// patron2 = 48px
+// patron3 = 96px
 let posicionX = 0;
 
 // es escritorio
 let esMovil = false;
-let lienzoAncho = 891;
-let lienzoAlto = 891;
-let patronAncho = 33; // 990 / 30 = 33
-let breakpoints = [0, 99, 297, 495, 693, 891, 990];
+let lienzoAncho = 720;
+let lienzoAlto = 720;
+let patronAncho = 24; // 720 / 30 = 24
+let breakpoints = [0, 72, 144, 216, 288, 360, 432, 504, 576, 648, 720];
 let paletasArray;
-
+let generados = 0;
 
 
 function preload() {
-    tablaPaletas = loadTable('paletas_pinturas.csv', 'csv', 'header');
-
+    tablaPaletas = loadTable('paletas.csv', 'csv', 'header');
 }
 
 function setup() {
@@ -21,10 +25,11 @@ function setup() {
     if (windowWidth <= 600) {
         esMovil = true;
         let container = select('#canvas-container');
-        container.style('width', '400px');
-        container.style('height', '400px');
+        container.style('width', '360px');
+        container.style('height', '360px');
         noLoop();
     }
+
     var canvas = createCanvas(lienzoAncho, lienzoAlto);
     canvas.parent('canvas-container');
     background(100);
@@ -32,12 +37,8 @@ function setup() {
 
     const botonGenerar = select('#generate');
     botonGenerar.mousePressed(() => {
-        generarBloque0();
-        generarBloque1();
-        generarBloque2();
-        generarBloque1();
-        generarBloque0();
-        posicionX = 0;
+        generarTodo();
+        generados++;
     });
 
     const botonDescargar = select('#download');
@@ -63,9 +64,50 @@ function estructuraLienzo(breakpoints) {
 
 }
 
-
 // funcion general de generar patrones
 function generarTodo() {
+    console.log(generados);
+    let paletas = [];
+    if (generados < 5) {
+        // divide the array in chunks of 5 consecutive subarray, then select a random chunk
+        let paletasChunks = [];
+        for (let i = 0; i < paletasArray.length; i += 5) {
+            let chunk = paletasArray.slice(i, i + 5);
+            paletasChunks.push(chunk);
+        }
+        paletas = randomElement(paletasChunks);
+    }
+
+    if (generados >= 5) {
+        let paletasRandom = [];
+        for (let i = 0; i < 5; i++) {
+            let paleta = randomElement(paletasArray);
+            paletasRandom.push(paleta);
+        }
+        paletas = paletasRandom;
+    }
+
+    // if (generados >= 10) {
+    //     // combine elements from different paletas subarrays
+    //     let paletasRandom = [];
+    //     for (let i = 0; i < 5; i++) {
+    //         let paleta = [];
+    //         for (let j = 0; j < paletasArray.length; j++) {
+    //             paleta.push(paletasArray[j][i]);
+    //         }
+    //         paletasRandom.push(paleta);
+    //     }
+
+    // }
+
+    generarBloque0(paletas);
+    generarBloque1(paletas);
+    generarBloque2(paletas);
+    generarBloque1(paletas);
+    generarBloque0(paletas);
+    posicionX = 0;
+}
+function generarRandom() {
     posicionX = 0;
     for (let i = 0; i < nPatrones; i++) {
         let patrones = [patron0];
@@ -75,56 +117,59 @@ function generarTodo() {
     }
 }
 
-function generarBloque0() {
+// funciones de generacion de bloques
+function generarBloque0(paletas) {
     for (let i = 0; i < 3; i++) {
-        patron0(posicionX, paletasPatron0[i]);
+        patron0(posicionX, paletas[i]);
     }
 }
 
-function generarBloque1() {
-    patron1(posicionX, paletasPatron1[0]);
-    patron0(posicionX, paletasPatron0[3]);
-    patron1(posicionX, paletasPatron1[0]);
+function generarBloque1(paletas) {
+    patron1(posicionX, paletas[4]);
+    patron0(posicionX, paletas[3]);
+    patron1(posicionX, paletas[4]);
+
+    // patron1(posicionX, paletasPatron1[0]);
+    // patron0(posicionX, paletasPatron0[3]);
+    // patron1(posicionX, paletasPatron1[0]);
 }
 
-function generarBloque2() {
-    for (let i = 0; i < 2; i++) {
-        patron2(posicionX, paletasPatron0[i]);
-    }
+function generarBloque2(paletas) {
+    patron2(posicionX, paletas[0]);
+    patron0(posicionX, paletas[1]);
+    patron3(posicionX, paletas[3]);
+    patron0(posicionX, paletas[1]);
+    patron2(posicionX, paletas[0]);
 
-    patron3(posicionX, paletasPatron0[3]);
-
-    for (let i = 1; i >= 0; i--) {
-        patron2(posicionX, paletasPatron0[i]);
-    }
+    // patron2(posicionX, paletasPatron0[0]);
+    // patron0(posicionX, paletasPatron0[1]);
+    // patron3(posicionX, paletasPatron0[3]);
+    // patron0(posicionX, paletasPatron0[1]);
+    // patron2(posicionX, paletasPatron0[0]);
 }
 
 // patrones de franjas
-
 function patron0(inicio, paleta) {
-    let ancho = 33;
+    //  x1 = 24
+    let ancho = 24;
     // 1, 1, 1, 1, 3, 1, 1, 1, 1 = 12 which are the units of the width
-    // 3, 3, 3, 3, 9, 3, 3, 3, 3 = 33 which is a multiple of 3
-    // 0, 3, 6, 9, 12, 21, 24, 27, 30, 33 which are the partial sums
+    // 2, 2, 2, 2, 6, 2, 2, 2, 2 = 24 which is a multiple of 3
+    // partial sums: 0, 2, 4, 6, 8, 16, 18, 20, 22, 24
     for (let i = 0; i < ancho; i += 1) {
-        if (i < 3 || i >= 30) {
+        if (i < 3 || i >= 21) {
             stroke(paleta[0]);
             line(i + inicio, 0, i + inicio, height);
         }
-        if (i >= 3 && i < 6 || i >= 27 && i < 30) {
+        if (i >= 3 && i < 6 || i >= 18 && i < 21) {
             stroke(paleta[1]);
             line(i + inicio, 0, i + inicio, height);
         }
-        if (i >= 6 && i < 9 || i >= 24 && i < 27) {
+        if (i >= 6 && i < 9 || i >= 15 && i < 18) {
             stroke(paleta[2]);
             line(i + inicio, 0, i + inicio, height);
         }
-        if (i >= 9 && i < 12 || i >= 21 && i < 24) {
+        if (i >= 9 && i < 15) {
             stroke(paleta[3]);
-            line(i + inicio, 0, i + inicio, height);
-        }
-        if (i >= 12 && i < 21) {
-            stroke(paleta[4]);
             line(i + inicio, 0, i + inicio, height);
         }
 
@@ -134,53 +179,53 @@ function patron0(inicio, paleta) {
 }
 
 
-
 function patron1(inicio, paleta) {
-    let ancho = 81;
-    // 3, 3, 3, 63, 3, 3, 3 = 81
+    // x3 = 72
+    let ancho = 72;
+    // 2, 2, 2, 60, 2, 2, 2 = 72
     for (let i = 0; i < ancho; i += 1) {
-        if (i < 3 || i >= 78) {
+        if (i < 2 || i >= 70) {
             stroke(paleta[0]);
             line(i + inicio, 0, i + inicio, height);
         }
-        if (i >= 3 && i < 6 || i >= 75 && i < 78) {
+        if (i >= 2 && i < 4 || i >= 68 && i < 70) {
             stroke(paleta[1]);
             line(i + inicio, 0, i + inicio, height);
         }
-        if (i >= 6 && i < 9 || i >= 72 && i < 75) {
+        if (i >= 4 && i < 6 || i >= 66 && i < 68) {
             stroke(paleta[2]);
             line(i + inicio, 0, i + inicio, height);
         }
-        if (i >= 9 && i < 72) {
+        if (i >= 6 && i < 66) {
             stroke(paleta[3]);
             line(i + inicio, 0, i + inicio, height);
         }
 
     }
-    stroke(0)
     posicionX = posicionX + ancho;
-    line(posicionX, 0, posicionX, height)
-    posicionX = posicionX + 1;
+
 
 }
 
 function patron2(inicio, paleta) {
-    let ancho = 66;
-    // 6, 6, 6, 6, 18, 6, 6, 6, 6 = 66
+    // x2 = 48
+    let ancho = 48;
+    // 4, 4, 4, 4, 12, 4, 4, 4, 4 = 48
+
     for (let i = 0; i < ancho; i += 1) {
-        if (i < 6 || i >= 60) {
+        if (i < 4 || i >= 44) {
             stroke(paleta[0]);
             line(i + inicio, 0, i + inicio, height);
         }
-        if (i >= 6 && i < 12 || i >= 54 && i < 60) {
+        if (i >= 4 && i < 8 || i >= 40 && i < 44) {
             stroke(paleta[1]);
             line(i + inicio, 0, i + inicio, height);
         }
-        if (i >= 12 && i < 18 || i >= 48 && i < 54) {
+        if (i >= 8 && i < 12 || i >= 36 && i < 40) {
             stroke(paleta[2]);
             line(i + inicio, 0, i + inicio, height);
         }
-        if (i >= 18 && i < 48) {
+        if (i >= 12 && i < 36) {
             stroke(paleta[3]);
             line(i + inicio, 0, i + inicio, height);
         }
@@ -192,22 +237,23 @@ function patron2(inicio, paleta) {
 }
 
 function patron3(inicio, paleta) {
-    let ancho = 132;
-    // 11, 11, 11, 66, 11, 11, 11 = 132
+    // x4 = 96
+    let ancho = 96;
+    // 8, 8, 8, 48, 8, 8, 8 = 96
     for (let i = 0; i < ancho; i += 1) {
-        if (i < 11 || i >= 121) {
+        if (i < 8 || i >= 88) {
             stroke(paleta[0]);
             line(i + inicio, 0, i + inicio, height);
         }
-        if (i >= 11 && i < 22 || i >= 110 && i < 121) {
+        if (i >= 8 && i < 16 || i >= 80 && i < 88) {
             stroke(paleta[1]);
             line(i + inicio, 0, i + inicio, height);
         }
-        if (i >= 22 && i < 33 || i >= 99 && i < 110) {
+        if (i >= 16 && i < 24 || i >= 72 && i < 80) {
             stroke(paleta[2]);
             line(i + inicio, 0, i + inicio, height);
         }
-        if (i >= 33 && i < 99) {
+        if (i >= 24 && i < 72) {
             stroke(paleta[3]);
             line(i + inicio, 0, i + inicio, height);
         }
@@ -215,38 +261,6 @@ function patron3(inicio, paleta) {
     }
     posicionX = posicionX + ancho;
 
-}
-
-
-// colores
-const paletasPatron0 = [
-    ["#FF897A", "#DB3E43", "#A0030F", "#560004", "#40050C"],
-    ["#BED480", "#63A46A", "#2C5C25", "#043816", "#03211A"],
-    ["#FAB4A6", "#F14065", "#D93E5E", "#87152B", "#451219"],
-    ["#84BBD4", "#128CB6", "#0074A5", "#003873", "#000651"]
-
-]
-
-const paletasPatron1 = [
-    ["#4A6046", "#DDB092", "#D74B66", "#002E41"],
-];
-
-function patronOld1(inicio) {
-    let paleta = paletasRandom();
-    let franja = franjasRandom();
-    for (let i = 0; i < patronAncho; i += 1) {
-
-        if (i < 20 || i >= 70) {
-            stroke(franja);
-            line(i + inicio, 0, i + inicio, height);
-        }
-        if (i >= 20 && i < 70) {
-            stroke(paleta[funcionEscalonada(i, 20, 10)]);
-            line(i + inicio, 0, i + inicio, height);
-        }
-
-    }
-    posicionX = posicionX + 90;
 }
 
 
@@ -278,7 +292,6 @@ function patronOld2(inicio) {
 
     posicionX = posicionX + 90;
 
-
 }
 
 // adornos
@@ -308,34 +321,11 @@ function adorno1(inicio, paleta) {
 
 // colores
 
-function paletasRandom() {
-    const paletas = [
-        ['red', 'orange', 'yellow', 'orange', 'red'],
-        ['blue', 'skyblue', 'white', 'skyblue', 'blue'],
-        ['green', 'lightgreen', 'white', 'lightgreen', 'green'],
-    ];
-    return paletas[Math.floor(Math.random() * paletas.length)];
-}
-
-function franjasRandom() {
-    const franjas = ['skyblue', 'crimson', 'green', "#B13166", "#F81868", "#F63B3A", "#F35FBA", "#6F1A65", "#013AA3", "#F00002", "#13B2CB", "#F93EA1", "#11C606"];
-    return franjas[Math.floor(Math.random() * franjas.length)];
-
-}
-
-function paletasRandom2() {
-    const paletas = [
-        ['red', 'orange', 'yellow'],
-        ['blue', 'skyblue', 'white'],
-        ['green', 'lightgreen', 'white'],
-        ["#E40003", "#FF4761", "#FDA9D7"], // rojos a rosados
-        ["#0251A3", "#03ACC7", "#8BFDDD"], // azules a celestes
-
-    ];
-    return paletas[Math.floor(Math.random() * paletas.length)];
-}
-
-
+paletasSaturadas = [
+    ['red', 'orange', 'yellow', 'orange', 'red'],
+    ['blue', 'skyblue', 'white', 'skyblue', 'blue'],
+    ['green', 'lightgreen', 'white', 'lightgreen', 'green'],
+];
 
 // utilities functions
 function funcionEscalonada(x, inset, step) {
@@ -343,6 +333,6 @@ function funcionEscalonada(x, inset, step) {
 }
 
 function randomElement(array) {
-    let element = array[Math.floor(Math.random * array.length)];
+    let element = array[Math.floor(Math.random() * array.length)];
     return element;
 }
